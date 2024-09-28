726034fcb60b7e18248876841bb5a923a703dd5d
Swapnil Nimbalkar <swapnilnimbalkarofficial@gmail.com>
2024-09-29 00:48:43 +0530

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,20 @@
+public class Pattern1 {
+    public static void main(String args[]) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j <= n; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
+
+// output
+/*
+ * 12345
+ * 2345
+ * 345
+ * 45
+ * 5
+ */
